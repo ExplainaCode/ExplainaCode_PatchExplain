99448ba6e8f061120fd76858a8010673a0ff85c8
Suwan-Aiken <suwan.sankaja@gmail.com>
2024-11-04 06:51:57 +0530

    added removed files

---
+++ setup_venv.bat
@@ -0,0 +1,11 @@
+@echo off
+
+:: Navigate to the project directory
+@REM cd /path/to/your/project
+
+:: Create virtual environment
+python -m venv new_venv
+
+:: Activate virtual environment
+
+echo Virtual environment created
